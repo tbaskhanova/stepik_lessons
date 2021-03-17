d105429e8d346a6c88f3e473bb04f9012543192b
Tamara <t_baskhanova@rambler.ru>
2021-03-17 19:58:33 +0500

    homework5

--- module_5/test_product_page.py
+++ module_5/test_product_page.py
@@ -6,7 +6,8 @@ from faker import Faker
 
 
 class TestProductPage:
-    @pytest.mark.parametrize('promo_offer',["offer0", "offer1", "offer2", "offer3", "offer4", "offer5", "offer6", pytest.param("offer7", marks=pytest.mark.xfail), "offer8","offer9"])
+    @pytest.mark.parametrize('promo_offer',["offer0", "offer1", "offer2", "offer3", "offer4", "offer5", "offer6",
+                                            pytest.param("offer7", marks=pytest.mark.xfail), "offer8","offer9"])
     def test_guest_can_add_product_to_basket(self,browser,promo_offer):
         # Data
         link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo={promo_offer}"
